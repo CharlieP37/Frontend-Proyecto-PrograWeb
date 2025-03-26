234c9a59e2ee12dcbd4b9b1b4b26f5d7f1692e28
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2025-03-26 00:51:17 -0600

    Stylesheet & Icon pack
    Added new stylessheet as primary replacing App.css and new icon pack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import './components/styles.css';
 import LoginPage from "./sites/loginscreen";
 import HomePage from "./sites/homescreen";
 
