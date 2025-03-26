1566f4cd7bc80c48c45bb0b5237586a72d7479a0
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2025-03-25 21:24:16 -0600

    Component Library Test
    Calendar Picker component libary test usage from Prime React

--- src/sites/homescreen.js
+++ src/sites/homescreen.js
@@ -1,5 +1,26 @@
+import React, { useState } from "react";
+import { Calendar } from 'primereact/calendar';
+
 function HomeScreen(){
-    return (<p>Pagina de Inicio</p>);
+    
+    const [date, setDate] = useState(null);
+
+    const dateTemplate = (date) => {
+        if (date.day > 10 && date.day < 15) {
+            return (
+                <strong style={{ textDecoration: 'line-through' }}>{date.day}</strong>
+            );
+        }
+
+        return date.day;
+    }
+
+    return (
+        <div className="card flex justify-content-center">
+            <Calendar value={date} onChange={(e) => setDate(e.value)} dateTemplate={dateTemplate} />
+        </div>
+    );
+
 };
 
 export default HomeScreen;
