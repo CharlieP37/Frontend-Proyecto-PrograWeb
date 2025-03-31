ed2144ebaacb8a87f5dba612b227f68915c2e813
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2025-03-31 00:20:50 -0600

    Button Component
    Creation of a single custom button basic component

---
+++ src/components/basic/button.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./button.css";
+
+function CustomButton({ type, atributes, ...rest}){
+    const [isHovered, setIsHovered] = useState(false);
+
+    return(
+        <div className="button-container" style={{ backgroundColor: isHovered ? "var(--font-primary)" : "var(--detail1)" }} id={atributes.name + "Container"}>
+            <button className ="custom-button" type={type} id={atributes.name} value={atributes.value} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} {...rest}>{atributes.text}</button>
+        </div>
+    );
+
+};
+
+export default CustomButton;
