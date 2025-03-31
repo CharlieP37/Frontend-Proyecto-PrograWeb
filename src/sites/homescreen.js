2fc3efdf8a63e9f67e3fc9104afac667560f9a14
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2025-03-31 01:14:48 -0600

    Homescreen page Design
    Design of the Home screen page with its components

--- src/sites/homescreen.js
+++ src/sites/homescreen.js
@@ -1,7 +1,33 @@
+import NavBarComponent from "../components/basic/navbar.js";
+import LeftMenuComponent from "../components/full/leftmenu.js"
+import RightMenuComponent from "../components/full/rightmenu.js"
+import ActionBarComponent from "../components/basic/actionbar.js";
+import "./homescreen.css";
+
 function HomeScreen(){
 
-    return(<p>Pagina de Inicio</p>);
-
+    return (
+        <div className="main-homescreen-container">
+            <div className="navbar-location-container">
+                <NavBarComponent/>
+            </div>
+            <div className="menu-location-container">
+                <div className="left-menu-location-container">
+                    <LeftMenuComponent/>
+                </div>
+                <div className="right-menu-location-container">
+                    <RightMenuComponent/>
+                </div>
+            </div>
+            <div className="actionbar-location-container">
+                <ActionBarComponent/>
+            </div>
+            <div className="credits-location-container">
+                <p>© Carlos Pop & Diego Gil. Universidad Rafael Landívar. 2025.</p>
+            </div>
+        </div>
+    );
+    
 };
 
 export default HomeScreen;
