af80fcab7765ae5c6fdbe1b1313bf0dc07d32496
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2025-03-31 01:16:37 -0600

    Loginscreen page Design
    Design of the Home screen page with its components

--- src/sites/loginscreen.js
+++ src/sites/loginscreen.js
@@ -1,5 +1,28 @@
+import LogoImg from '../assets/ModifyLogoAlt1.svg';
+import SignUpOptionsComponent from "../components/full/signupoptions.js";
+import "./loginscreen.css";
+
 function LoginScreen(){
-    return(<p>Pagina de Inicio de Sesion</p>);
+    
+    return(
+        <div className="main-login-container">
+            <div className="content-container">
+                <div className="left-container">
+                    <img id="mainLogo" src={LogoImg} alt="Logo"/>
+                </div>
+                <div className="right-container">
+                    <h2 className="subtitle-font-style">Tu estado de ánimo, tu música. La playlist perfecta para cada emoción.</h2>
+                    <div className="login-options-container">
+                        <SignUpOptionsComponent/>
+                    </div>
+                </div>
+            </div>
+            <div className="credits-location-container">
+                <p>© Carlos Pop & Diego Gil. Universidad Rafael Landívar. 2025.</p>
+            </div>
+        </div>
+    );
+    
 };
 
 export default LoginScreen;
