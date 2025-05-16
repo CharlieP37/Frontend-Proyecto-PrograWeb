842c82a29f1347422770dd9767579fc21dbfddd1
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2025-05-15 18:56:06 -0600

    Various components style update
    Update style to various components

--- src/components/basic/input.js
+++ src/components/basic/input.js
@@ -9,7 +9,7 @@ function CustomInput({ type, atributes, value, onChange, ...rest}){
         <div className="input-container" id={atributes.name + "Container"}>
             <FloatLabel className="custom-floatlabel" id={atributes.name + "FloatLabel"}>
                 <InputText className ="custom-input" type={type} id={atributes.name} name={atributes.name} value={value} onChange={(e) => {if (onChange) {onChange(e);}}} {...rest}/>
-                <label id={atributes.name + "Label"} htmlFor={atributes.name}>{atributes.label}</label>
+                <label className="custom-label" id={atributes.name + "Label"} htmlFor={atributes.name}>{atributes.label}</label>
             </FloatLabel>
         </div>
     );
