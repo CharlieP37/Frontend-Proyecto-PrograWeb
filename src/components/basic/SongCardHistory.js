42186a1a69755600ba17f74e215cac6a0a97c1df
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2025-05-17 03:33:55 -0600

    Spotify Frame Player
    Added a play button to listen the recommended music on history records.

--- src/components/basic/SongCardHistory.js
+++ src/components/basic/SongCardHistory.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Button } from 'primereact/button';
 import { Tooltip } from 'primereact/tooltip';
+import DialogHeader from '../basic/dialogheader';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import './SongCardHistory.css';
 import { setRecommendationFeedback } from '../../services/api.js';
+import './SongCardHistory.css';
 
 const SongCardHistory = ({
   id,
@@ -19,6 +20,7 @@ const SongCardHistory = ({
 }) => {
   const [reaction, setReaction] = useState(feedback);
   const [Id] = useState(id);
+  const [dialogVisible, setDialogVisible] = useState(false);
 
   const handleSetRecommendationFeedback = async (value) => {
     try {
@@ -53,89 +55,124 @@ const SongCardHistory = ({
     }
   };
 
+  const handleMusic = () => {
+    setDialogVisible(true);
+  };
+
+  function extractSpotifyTrackId(url) {
+    const match = url.match(/track\/([a-zA-Z0-9]+)/);
+    return match ? match[1] : null;
+  }
+
   const likeButtonClass = reaction === 'like' ? 'song-history-active-like' : reaction === 'dislike' ? 'song-history-inactive' : '';
   const dislikeButtonClass = reaction === 'dislike' ? 'song-history-active-dislike' : reaction === 'like' ? 'song-history-inactive' : '';
 
   return (
-    <div className="song-history-container">
-      <div className="song-history-main">
-        <div className="song-history-image-container">
-          {songImageBase64 ? (
-            <img 
-              src={songImageBase64} 
-              alt={`Portada de ${songName}`} 
-              className="song-history-image"
-            />
-          ) : (
-            <div className="song-history-image-placeholder">
-              <i className="pi pi-music" />
-            </div>
-          )}
-        </div>
+    <>
+      <div className="song-history-container">
+        <div className="song-history-main">
+          <div className="song-history-image-container">
+            {songImageBase64 ? (
+              <img 
+                src={songImageBase64} 
+                alt={`Portada de ${songName}`} 
+                className="song-history-image"
+              />
+            ) : (
+              <div className="song-history-image-placeholder">
+                <i className="pi pi-music" />
+              </div>
+            )}
+          </div>
 
-        <div className="song-history-text-info">
-          <Tooltip target=".song-history-artist" />
-          <Tooltip target=".song-history-title" />
-          
-          <p 
-            className="song-history-artist"
-            data-pr-tooltip={artist}
-            data-pr-position="top"
-          >
-            {artist}
-          </p>
-          <a
-            href={url}
-            target='_blank'
-            rel="noopener noreferrer"
-            className='song-history-link'
-          >
-            <h3 
-              className="song-history-title"
-              data-pr-tooltip={songName}
+          <div className="song-history-text-info">
+            <Tooltip target=".song-history-artist" />
+            <Tooltip target=".song-history-title" />
+            
+            <p 
+              className="song-history-artist"
+              data-pr-tooltip={artist}
               data-pr-position="top"
             >
-              {songName}
-            </h3>
-          </a>
-        </div>
+              {artist}
+            </p>
+            <a
+              href={url}
+              target='_blank'
+              rel="noopener noreferrer"
+              className='song-history-link'
+            >
+              <h3 
+                className="song-history-title"
+                data-pr-tooltip={songName}
+                data-pr-position="top"
+              >
+                {songName}
+              </h3>
+            </a>
+          </div>
 
-        <div className="song-history-emotion-container">
-          {emotionImageBase64 ? (
-            <img 
-              src={emotionImageBase64} 
-              alt="Emoción de la canción"
-              className="song-history-emotion-image"
+          <div className="song-history-emotion-container">
+            {emotionImageBase64 ? (
+              <img 
+                src={emotionImageBase64} 
+                alt="Emoción de la canción"
+                className="song-history-emotion-image"
+              />
+            ) : (
+              <div className="song-history-emotion-placeholder">
+                <i className="pi pi-image" />
+              </div>
+            )}
+          </div>
+          <div className="song-history-actions">
+            <Button
+              icon="pi pi-play-circle"
+              className='song-listen-window-button'
+              onClick={handleMusic}
+              tooltip='Reproducir Canción'
+              tooltipOptions={{position: "top"}}
             />
-          ) : (
-            <div className="song-history-emotion-placeholder">
-              <i className="pi pi-image" />
-            </div>
-          )}
-        </div>
+            <Button
+              icon="pi pi-heart" 
+              className={`song-history-like-button ${likeButtonClass}`}
+              onClick={handleLike}
+              tooltip={reaction === 'like' ? "Quitar like" : "Me gusta"}
+              tooltipOptions={{ position: 'top' }}
+            />
+            <Button 
+              icon="pi pi-ban"
+              className={`song-history-dislike-button ${dislikeButtonClass}`}
+              onClick={handleDislike}
+              tooltip={reaction === 'dislike' ? "Quitar dislike" : "No me gusta"}
+              tooltipOptions={{ position: 'top' }}
+            />
+          </div>
 
-        <div className="song-history-actions">
-          <Button
-            icon="pi pi-heart" 
-            className={`song-history-like-button ${likeButtonClass}`}
-            onClick={handleLike}
-            tooltip={reaction === 'like' ? "Quitar like" : "Me gusta"}
-            tooltipOptions={{ position: 'top' }}
-          />
-          <Button 
-            icon="pi pi-ban"
-            className={`song-history-dislike-button ${dislikeButtonClass}`}
-            onClick={handleDislike}
-            tooltip={reaction === 'dislike' ? "Quitar dislike" : "No me gusta"}
-            tooltipOptions={{ position: 'top' }}
-          />
-        </div>
-
-        <div className="song-history-date-container">
-          <p className="song-history-date">{date}</p>
+          <div className="song-history-date-container">
+            <p className="song-history-date">{date}</p>
+          </div>
         </div>
       </div>
-    </div>
+      {dialogVisible && (
+        <DialogHeader
+          id={"playerspotify"}
+          visible={dialogVisible}
+          onHide={() => setDialogVisible(false)}
+          children={
+            <div className='music-player-preview-container'>
+              <iframe
+              className='spotify-iframe-custom'
+              src={`https://open.spotify.com/embed/track/${extractSpotifyTrackId(url)}`}
+              frameBorder="0"
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+              title="Spotify Embed"
+              ></iframe>
+            </div>
+          }
+        />)}
+    </>
   );
 };
 
